2b7eb68c6d1c3a6a94b16a92b726f354e21bb16b
Laky <75489200+dev-Laky@users.noreply.github.com>
2021-08-08 17:08:32 +0200

    Update laky_main.py

--- Runde1/Aufgabe1/laky_main.py
+++ Runde1/Aufgabe1/laky_main.py
@@ -3,7 +3,8 @@ INPUT_PATH = "input"
 
 class Solver:
     def __init__(self):
-        pass
+        self.list_1 = None
+        self.list_2 = None
 
     def main(self):
         self.read_inputs()
@@ -11,10 +12,11 @@ class Solver:
     def read_inputs(self):
         file_num = 0
         with open(f"{INPUT_PATH}/raetsel{file_num}.txt", "r", encoding="utf-8") as file:
-            list_1 = file.readline().split()
-            list_2 = file.readline().split()
-            print(list_1)
-            print(list_2)
+            self.list_1 = file.readline().split()
+            self.list_2 = file.readline().split()
+            print(self.list_1)
+            print(self.list_2)
+
 
 if __name__ == '__main__':
     Solver().main()
