a3a1b3e6f0d16472fd020348b353e7362fad88ae
Laky <75489200+dev-Laky@users.noreply.github.com>
2021-08-08 17:38:57 +0200

    Update laky_main.py

--- Runde1/Aufgabe1/laky_main.py
+++ Runde1/Aufgabe1/laky_main.py
@@ -18,13 +18,20 @@ class Solver:
         for i in range(len(self.list_1)):
             for char in self.list_1[i]:
                 if char != "_":
+                    x = self.list_1[i].index(char)
                     print(self.list_1[i].index(char))
+                    for c in range(len(self.list_2)):
+                        try:
+                            if self.list_2[c].index(char) == x:
+                                print(f"found {self.list_1[i]} and {self.list_2[c]}")
+                        except ValueError:
+                            pass
 
         print(self.list_1)
         print(self.list_2)
 
     def read_inputs(self):
-        file_num = 0
+        file_num = 4
         with open(f"{INPUT_PATH}/raetsel{file_num}.txt", "r", encoding="utf-8") as file:
             self.list_1 = file.readline().split()
             self.list_2 = file.readline().split()
